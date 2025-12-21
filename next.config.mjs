5d80ac64a8b57cecca155b033c6dc038c1929388
yuu <yuuiti.htnk167@gmail.com>
2025-12-21 21:38:40 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
- 
-}
+};
 
-export default nextConfig
+export default nextConfig;
